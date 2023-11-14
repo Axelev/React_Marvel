7f1c236ef4006d41d5f6b11e005908e3c997c20d
Axelev <87axelev@gmail.com>
2023-11-14 21:05:46 +0100

    added loading spinner and error message

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
     constructor(props) {
@@ -11,13 +13,23 @@ class RandomChar extends Component {
     }
 
     state = {
-        char : {}
+        char : {},
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char});
+        this.setState({
+            char, 
+            loading: false});
+    }
+
+    onError = () => {
+        this.setState({ 
+            loading: false,
+            error: true});
     }
 
     updateChar = () => {
@@ -25,6 +37,7 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     descriptionCorrector = (text) => {
@@ -36,25 +49,19 @@ class RandomChar extends Component {
     }
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
-        
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? 
+                        <View char={char} 
+                              descriptionCorrector={this.descriptionCorrector}/> :
+                        null;
+       
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">{this.descriptionCorrector(description)}</p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -69,8 +76,30 @@ class RandomChar extends Component {
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
                 </div>
             </div>
-        )
+        )       
     }
+}
+
+const View = ({char, descriptionCorrector}) => {
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">{descriptionCorrector(description)}</p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;

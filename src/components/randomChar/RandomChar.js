58672170d6428e9b757aeebadcc6616aab3d1972
Axelev <87axelev@gmail.com>
2023-11-22 19:39:34 +0100

    added funcionality to TRY IT button

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,10 +7,6 @@ import Spinner from '../spinner/spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char : {},
@@ -19,6 +15,23 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    onCharUpdate = () => {
+        this.setState({
+            loading: true
+        })
+        this.updateChar();
+
+    }
 
     onCharLoaded = (char) => {
         this.setState({
@@ -70,7 +83,8 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main"
+                            onClick={this.onCharUpdate}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -82,10 +96,15 @@ class RandomChar extends Component {
 
 const View = ({char, descriptionCorrector}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+    let imgClassName = 'randomchar__img';
+
+    if (thumbnail.includes('image_not_available')) {
+        imgClassName += '__not_found';
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className={imgClassName}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">{descriptionCorrector(description)}</p>

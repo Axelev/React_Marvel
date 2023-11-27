2ec150f6de3df03aa6582a045b4a3652db4499c3
Axelev <87axelev@gmail.com>
2023-11-27 18:42:48 +0100

    added funcionality to CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,85 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import CharListItem from '../charListItem/charListItem';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
+import MarvelService from '../../services/MarvelService';
+import './charList.scss';
+
+
+class CharList extends Component {
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    updateList = () => {
+        this.marvelService.getAllCharacters()
+                          .then(this.listLoaded)
+                          .catch(this.onError)
+    }
+    
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false,
+        })
+    }
+
+    listLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false,
+        });
+
+        console.log(this.state.charList);
+    }
+
+    componentDidMount() {
+        this.updateList();
+    }
+
+    renderItems = (arr) => {
+        const items = arr.map(item => {
+            return (
+                <CharListItem 
+                    name={item.name}
+                    thumbnail={item.thumbnail}
+                    key={item.id}/>
+            )
+        })
+
+        return (
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                {items}
             </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+        )
+    }
+
+    render() {
+
+        const {charList, loading, error} = this.state;
+        const spinner = loading? <Spinner/> : null;
+        const errorMessage = error? <ErrorMessage/> : null;
+        const content = !error || loading ? this.renderItems(charList): null;
+
+
+
+        return (
+            <div className="char__list">
+                {spinner}
+                {errorMessage}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+    
 }
 
 export default CharList;
